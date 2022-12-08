080d0208bb43bd3014c24ae62f25b267ffd8b298
kjacques <kjacques@mashbo.com>
2022-12-08 16:01:02 +0000

    Delete duplicate javascript files

--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./app");
-require("./routes/digitalSign");
-require("./routes/electronicSign");
-require("./routes/index");
-//# sourceMappingURL=index.js.map
